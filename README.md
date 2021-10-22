8049d73d0ebf6a866c8f9e3a18e4eb2f642ac192
Karl Dreher <karldreher@outlook.com>
2021-10-21 20:11:42 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # cloudflare-pages-delete-revisions
+
+This project contains a Python script for deleting preview deployments of **all** Cloudflare Pages projects within an account.  It is intended to be run as a scheduled job, e.g. with Github Actions.
+
+## Usage
+
+```bash
+pip3 install -r requirements.txt
+python3 deletePreviewDeployments.py
+
+```
+This assumes that the following environment variables are set: 
+`ACCOUNT_ID` 
+`AUTH_EMAIL` 
+`API_KEY` 
---
+++ deletePreviewDeployments.py
@@ -0,0 +1,36 @@
+import requests
+
+ACCOUNT_ID = 
+AUTH_EMAIL = 
+API_KEY = 
+
+globalHeaders = {
+    "content-type": "application/json;charset=UTF-8",
+    "X-Auth-Email": AUTH_EMAIL,
+    "X-Auth-Key": API_KEY
+}
+
+
+def getProjects():
+    projects = requests.get("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/pages/projects", headers=globalHeaders)
+    return projects.json()
+
+def notAlias(deployment):
+    if deployment["aliases"] == None:
+        return True
+
+def deleteProjectRevisions(projectName):
+    deployments = requests.get("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/pages/projects/" + projectName + "/deployments", headers=globalHeaders)
+    deploymentsToDelete = filter(notAlias,deployments.json()["result"])
+    for deployment in deploymentsToDelete:
+        endpoint = "https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/pages/projects/" + projectName + "/deployments/" + deployment["id"]
+        deleteRequest = requests.delete(endpoint, headers=globalHeaders)
+        print(deleteRequest.json())
+
+
+if __name__=="__main__":
+
+    projects = getProjects()
+    for project in projects["result"]:
+        projectName = project["name"]
+        deleteProjectRevisions(projectName)
