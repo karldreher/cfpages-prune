66829d2621074488516326d960812df17fad2dc3
Karl Dreher <karldreher@outlook.com>
2023-02-19 20:27:35 -0700

    fix: limit secrets exposure in logging

--- cf_pages_delete_previews/config.py
+++ cf_pages_delete_previews/config.py
@@ -20,5 +20,25 @@ class Configuration:
             self.account_id)
 
         if not all(list(self.__dict__.values())):
-            log.error("One or more values are not set properly.  CF_account_id, CF_auth_email, and CF_api_key must be set as environment variables.")
-            log.error("Current configuration: %s",self.__dict__)
+            log.error("One or more values are not set properly.  CF_ACCOUNT_ID, CF_AUTH_EMAIL, and CF_API_KEY must be set as environment variables.")
+            log.error("Current configuration: %s",redact(self.__dict__))
+
+def redact(dict:dict):
+    '''
+    Redact a dictionary to remove api keys from logs.
+    '''
+    # This is an alternative implementation from the reccomended approach,
+    # which is to create a custom formatter.
+    # This is OK for now.
+    newDict = {}
+    for k in dict:
+        if k == 'api_key':
+            newDict['api_key']='<REDACTED>'
+        elif k == 'headers':
+            # We don't need this because it is derived from the environment vars.
+            pass
+        else:
+            newDict[k]=dict.get(k)
+
+
+    return newDict
