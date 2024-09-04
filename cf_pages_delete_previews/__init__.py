fbb999cf1227352f63c650f2362cf0d174012b73
Karl Dreher <karldreher@outlook.com>
2024-09-03 19:43:24 -0700

    feat: configure logging with env

--- cf_pages_delete_previews/__init__.py
+++ cf_pages_delete_previews/__init__.py
@@ -2,9 +2,10 @@ __version__ = '1.1.0'
 
 import logging
 import sys
-
+import os
+log_level = os.getenv('LOG_LEVEL', 'INFO')
 file_handler = logging.FileHandler(filename='cfpages-prune' + '.log')
 stdout_handler = logging.StreamHandler(sys.stdout)
 logging_handlers = [file_handler, stdout_handler]
 logging.basicConfig(handlers=logging_handlers, format='%(asctime)s %(levelname)s - %(message)s',
-                    level='INFO', datefmt='%Y-%m-%d %I:%M:%S %p')
+                    level=log_level, datefmt='%Y-%m-%d %I:%M:%S %p')
