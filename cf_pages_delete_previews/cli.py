e243322451ca1206cfb2c0d8950433a1ef8ee7a6
Karl Dreher <8273552+karldreher@users.noreply.github.com>
2024-09-03 21:04:44 -0700

    feat: add new options (#31)
    * feat: add list option
    
    * feat: configure logging with env
    
    * ci: bump actions versions
    
    * feat: add option to force delete
    
    * test: update pytest and silence warnings
    
    * chore: bump deps
    
    * chore: lock poetry
    
    * fix: file
    
    * fix: syntax
    
    * fix: syntax
    
    * test: fix test
    
    * ci: update python-semantic-release
    
    * ci: fix syntax
    
    * docs: update docs
    
    * ci: how on earth did codeql get enabled
    
    * Revert "ci: how on earth did codeql get enabled"
    
    This reverts commit df036582912ddb59c04c49dc9db0b3defb273309.
    
    * ci: fix release preview
    
    * ci: fix syntax
    
    * fix: breaking changes
    
    * ci: remove preview
    
    * fix: remove option
    
    * fix: remove deprecated options

--- cf_pages_delete_previews/cli.py
+++ cf_pages_delete_previews/cli.py
@@ -10,11 +10,13 @@ def main():
     """Console script for cf_pages_delete_previews."""
     argparser = argparse.ArgumentParser()
     # while it looks strange and out of order, args must be in the order you want them to appear in --help
+    argparser.add_argument("--list-projects", action="store_true", default=False, required=False,help="List all projects and exit.  Ignores other arguments.")
     ex_group_redact = argparser.add_mutually_exclusive_group()
     ex_group_redact.add_argument("--projects","--project",action="store",required=False,help="Comma-delimited list of project names where revisions should be deleted.  Default: All projects")
     argparser.add_argument("--projectids","--projectid",action="store",required=False,help="Comma-delimited list of project IDs where revisions should be deleted.  Default: All projects")
     ex_group_redact.add_argument("--redact", action="store_true", default=False, required=False,
                         help="When \"--redact\" is used, project names will be replaced with IDs in log output.  Cannot be used in conjunction with --projects.")
+    argparser.add_argument("--force",action="store_true", default=False, required=False,help="Force deletion of all revisions, including those in production environments.  Use with extreme caution.")
     argparser.add_argument("--whatif", "--dry-run", action="store_true", default=False, required=False,
                         help="When \"--whatif\" or \"--dry-run\" is used, delete action will be skipped.")
 
@@ -24,6 +26,10 @@ def main():
     projectFilter = config.ProjectFilter(args)
 
     projects = lib.get_projects(cf_config)
+    if args.list_projects:
+        for project in projects:
+            print(f"{project['name']} ({project['id']})")
+        exit(0)
     if projects.__len__()!=0:
         for project in lib.filter_projects(projects,projectFilter):
             lib.delete_project_revisions(project, cf_config, args)
