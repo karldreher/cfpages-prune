f7ac139fe4c568e5a166a1d53526863573186d92
Karl Dreher <8273552+karldreher@users.noreply.github.com>
2024-09-04 20:56:32 -0700

    fix: misc fixes (#33)
    * chore: manually update versions
    
    * fix: semantic release
    
    * feat: add type hints
    
    * ci: enable pylint
    
    * ci: drop in ruff
    
    * ci: split up jobs
    
    * ci: fix name
    
    * lint: ruff fixes
    
    * lint: ruff fixes

--- cf_pages_delete_previews/lib.py
+++ cf_pages_delete_previews/lib.py
@@ -1,6 +1,7 @@
 import logging
-from concurrent.futures import ThreadPoolExecutor
 from argparse import Namespace
+from concurrent.futures import ThreadPoolExecutor
+
 import requests
 
 from cf_pages_delete_previews import config
@@ -49,7 +50,7 @@ def filter_projects(project_list:list,project_filter=config.ProjectFilter)->list
     # if no filter is specified, return all projects
     return project_list
 
-def get_deployments(project_name,cf_config:type[config.Configuration]):
+def get_deployments(project_name:str,cf_config:type[config.Configuration]):
     deployments = session.get(
         cf_config.account_url + "/pages/projects/" + project_name + "/deployments", headers=cf_config.headers, timeout=5)
     return deployments.json()
@@ -63,11 +64,10 @@ def delete_eligible(deployment:str,args:Namespace)->bool:
         return True
     return None
 
-def delete_single_revision(deployment:str, cf_config:type[config.Configuration], project, project_identifier, args):
+def delete_single_revision(deployment:str, cf_config:type[config.Configuration], project:str, project_identifier:str, args:Namespace):
     what_if = "Would take action: " if vars(args).get("whatif") else ""
 
-    log.info("%sDeleting deployment \'%s\' from project \'%s\'..." %
-    (what_if, deployment["id"], project_identifier))
+    log.info("{}Deleting deployment \'{}\' from project \'{}\'...".format(what_if, deployment["id"], project_identifier))
 
     delete_endpoint = cf_config.account_url + "/pages/projects/" + \
         project["name"] + "/deployments/" + deployment["id"]
@@ -77,23 +77,23 @@ def delete_single_revision(deployment:str, cf_config:type[config.Configuration],
         delete_request = session.delete(
             delete_endpoint, headers=cf_config.headers, timeout=5)
 
-        if delete_request.json()["success"] == True:
+        if delete_request.json()["success"] is True:
             log.info(
                 "Delete request for deployment '%s' was successful.", deployment["id"])
         else:
             log.error("Delete request for deployment '%s' was not successful.  Additional information from the request is included below.", deployment["id"])
             log.error(delete_request.json())
 
-def delete_project_revisions(project, cf_config:type[config.Configuration], args):
+def delete_project_revisions(project:str, cf_config:type[config.Configuration], args:Namespace):
     # although project_identifier allows redacting project name, it is still mandatory for api calls.
     project_identifier = project["id"] if vars(args).get("redact") else project["name"]
 
-    log.info("Started working on project %s with options: %s" % (project_identifier, vars(args)))
+    log.info(f"Started working on project {project_identifier} with options: {vars(args)}")
 
     deployments = get_deployments(project["name"],cf_config)
     for deployment in filter(lambda x: delete_eligible(x, args), deployments["result"]):
         with ThreadPoolExecutor(max_workers=3) as executor:
-            job = executor.submit(delete_single_revision, deployment, cf_config, project, project_identifier, args)
+            executor.submit(delete_single_revision, deployment, cf_config, project, project_identifier, args)
 
     if vars(args).get("whatif"):
         log.info("What if scenario: No action taken.")
