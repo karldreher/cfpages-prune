27d0ff04812cb24198a717263faaa989059523ad
Karl Dreher <karldreher@outlook.com>
2024-09-04 21:01:37 -0700

    chore: format

--- cf_pages_delete_previews/cli.py
+++ cf_pages_delete_previews/cli.py
@@ -1,4 +1,5 @@
 """Console script for cf_pages_delete_previews."""
+
 import argparse
 import logging
 import sys
@@ -6,20 +7,56 @@ import sys
 from cf_pages_delete_previews import config, lib
 
 log = logging.getLogger(__name__)
+
+
 def main():
     """Console script for cf_pages_delete_previews."""
     argparser = argparse.ArgumentParser()
     # while it looks strange and out of order, args must be in the order you want them to appear in --help
-    argparser.add_argument("--list-projects", action="store_true", default=False, required=False,help="List all projects and exit.  Ignores other arguments.")
+    argparser.add_argument(
+        "--list-projects",
+        action="store_true",
+        default=False,
+        required=False,
+        help="List all projects and exit.  Ignores other arguments.",
+    )
     ex_group_redact = argparser.add_mutually_exclusive_group()
-    ex_group_redact.add_argument("--projects","--project",action="store",required=False,help="Comma-delimited list of project names where revisions should be deleted.  Default: All projects")
-    argparser.add_argument("--projectids","--projectid",action="store",required=False,help="Comma-delimited list of project IDs where revisions should be deleted.  Default: All projects")
-    ex_group_redact.add_argument("--redact", action="store_true", default=False, required=False,
-                        help="When \"--redact\" is used, project names will be replaced with IDs in log output.  Cannot be used in conjunction with --projects.")
-    argparser.add_argument("--force",action="store_true", default=False, required=False,help="Force deletion of all revisions, including those in production environments.  Use with extreme caution.")
-    argparser.add_argument("--whatif", "--dry-run", action="store_true", default=False, required=False,
-                        help="When \"--whatif\" or \"--dry-run\" is used, delete action will be skipped.")
-
+    ex_group_redact.add_argument(
+        "--projects",
+        "--project",
+        action="store",
+        required=False,
+        help="Comma-delimited list of project names where revisions should be deleted.  Default: All projects",
+    )
+    argparser.add_argument(
+        "--projectids",
+        "--projectid",
+        action="store",
+        required=False,
+        help="Comma-delimited list of project IDs where revisions should be deleted.  Default: All projects",
+    )
+    ex_group_redact.add_argument(
+        "--redact",
+        action="store_true",
+        default=False,
+        required=False,
+        help='When "--redact" is used, project names will be replaced with IDs in log output.  Cannot be used in conjunction with --projects.',
+    )
+    argparser.add_argument(
+        "--force",
+        action="store_true",
+        default=False,
+        required=False,
+        help="Force deletion of all revisions, including those in production environments.  Use with extreme caution.",
+    )
+    argparser.add_argument(
+        "--whatif",
+        "--dry-run",
+        action="store_true",
+        default=False,
+        required=False,
+        help='When "--whatif" or "--dry-run" is used, delete action will be skipped.',
+    )
 
     args = argparser.parse_args()
     cf_config = config.Configuration()
@@ -30,11 +67,12 @@ def main():
         for project in projects:
             print(f"{project['name']} ({project['id']})")
         exit(0)
-    if projects.__len__()!=0:
-        for project in lib.filter_projects(projects,projectFilter):
+    if projects.__len__() != 0:
+        for project in lib.filter_projects(projects, projectFilter):
             lib.delete_project_revisions(project, cf_config, args)
     else:
         log.error("No projects found")
+
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,12 +2,11 @@ from cf_pages_delete_previews import config
 
 
 def test_redact():
-
     secretData = {
-        "api_key":"A_REALLY_BIG_SECRET",
-        "headers":{"apiKey":"A_REALLY_BIG_SECRET"},
-        "accountID":"12345",
-        "everythingElse":"Some Non Secret Stuff"
+        "api_key": "A_REALLY_BIG_SECRET",
+        "headers": {"apiKey": "A_REALLY_BIG_SECRET"},
+        "accountID": "12345",
+        "everythingElse": "Some Non Secret Stuff",
     }
-    assert config.redact(secretData).get("api_key")=='<REDACTED>'
+    assert config.redact(secretData).get("api_key") == "<REDACTED>"
     assert not config.redact(secretData).get("headers")
